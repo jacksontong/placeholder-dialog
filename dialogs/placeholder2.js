e6e228165dec2c633d9eac0e4f4692848dad2b82
Jackson Tong <tongtoan2704@gmail.com>
2016-10-19 11:07:17 +0700

    allow gt and lt tags

--- dialogs/placeholder2.js
+++ dialogs/placeholder2.js
@@ -40,7 +40,7 @@ function initializeList() {
 }
 
 CKEDITOR.dialog.add( 'placeholder2', function( editor ) {
-	var validNameRegex = /^[^\[\]<>]+$/;
+	var validNameRegex = /^[^\[\]]+$/;
 	var placeholderList = editor.config.placeholder2.placeholders.map(function(placeholder) {
 		return '<a href="javascript:void(0)" onclick="setPlaceholder(this); return false;" title="'+
 			placeholder.value +'">' + placeholder.label + '</a>';
